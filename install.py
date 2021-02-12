d8f93363513b4b54063f42fce09046ca952cf01a
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-02-12 12:01:55 -0600

    push

--- install.py
+++ install.py
@@ -7,7 +7,6 @@ Check the bashrc file to check if finder was previously installed
 """
 temp_file=os.path.expanduser('~')+"/"+ '.bashrc.temp'
 actual_file=os.path.expanduser('~')+"/"+ '.bashrc'
-os.system(f"rm -f {temp_file}")
 
 finder_installation_locations=[]
 fhr=open(os.path.expanduser('~')+"/"+ '.bashrc',"r")
@@ -16,20 +15,28 @@ for line in fhr:
         finder_installation_locations.append(line.strip().split("$PATH:")[-1])
 fhr.close()
 
+print(finder_installation_locations)
 remove_these_indices=[]
 for i,each_installation in enumerate(finder_installation_locations):
     check_this_file="software_identity"
     verify_contents="FINDER: An automated software package to annotate eukaryotic genes from RNA-Seq data and associated protein sequences - Banerjee et, al 2021"
+    #print(each_installation+"/"+check_this_file)
     if os.path.exists(each_installation+"/"+check_this_file)==True:
+        #print(open(each_installation+"/"+check_this_file).read().split("\n")[0])
+        #print(verify_contents)
         if verify_contents != open(each_installation+"/"+check_this_file).read().split("\n")[0]:
             remove_these_indices.append(i)
+        else:
+            print("Match found")
     else:
+        print("File not found")
         remove_these_indices.append(i)
 
+print(remove_these_indices)
 for i in remove_these_indices[::-1]:
     finder_installation_locations.pop(i)
 
-
+print(finder_installation_locations)
 if len(finder_installation_locations)>0:
     fhr=open(os.path.expanduser('~')+"/"+ '.bashrc',"r")
     fhw = open(os.path.expanduser('~')+"/"+ '.bashrc.temp',"w")
@@ -50,3 +57,8 @@ if len(finder_installation_locations)>0:
     os.system(cmd)
     
 os.system("./install.sh")
+
+
+
+
+

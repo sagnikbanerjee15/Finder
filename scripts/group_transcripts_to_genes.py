deaffa08195f97a057057eb2b7dd511c3845ed86
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2022-07-28 07:57:04 -0500

    Update group_transcripts_to_genes.py

--- scripts/group_transcripts_to_genes.py
+++ scripts/group_transcripts_to_genes.py
@@ -18,9 +18,46 @@ for line in fhr:
     if line[2] == "transcript":
         chromosome = line[0]
         if chromosome not in gene_info:
-            gene_info[chromosome] = {}
+            gene_info[chromosome] = []
         previous_transcript_id = line[-1].split("transcript_id")[-1].strip().split()[0].strip("\"")
         start = int(line[3])
         end = int(line[4])
-        gene_info[chromosome][previous_transcript_id] = [start,end]
+        strand = line[6]
+        gene_info[chromosome].append([previous_transcript_id,start,end,strand])
 fhr.close()
+
+# Merge transcripts into genes
+
+transcripts_grouped_into_genes = {}
+
+for chromosome in gene_info:
+    gene_number = 1
+    for row in gene_info[chromosome]:
+        previous_transcript_id, start, end, strand = row
+        transcript_incorporated = 0
+        for gene in transcripts_grouped_into_genes[chromosome]:
+            for transcript in transcripts_grouped_into_genes[chromosome][gene]:
+                transcript_start, transcript_end, transcript_strand = transcripts_grouped_into_genes[chromosome][gene][transcript]
+                if strand !="." and strand != transcript_strand: continue 
+                # Check if this transcript overlaps at least 80%
+                if start <= transcript_start <= end <= transcript_end:
+                    if ( end - transcript_start )/(end - start if (end-start)<(transcript_end-transcript_start) else (transcript_end-transcript_start) ) > 0.8:
+                        transcripts_grouped_into_genes[chromosome][gene].append(previous_transcript_id)
+                        transcript_incorporated = 1
+                elif transcript_start <= start <= transcript_end <= end:
+                    if ( transcript_end - start )/(end - start if (end-start)<(transcript_end-transcript_start) else (transcript_end-transcript_start) ) > 0.8:
+                        transcripts_grouped_into_genes[chromosome][gene].append(previous_transcript_id)
+                        transcript_incorporated = 1
+                elif start <= transcript_start <= transcript_end <= end:
+                    transcripts_grouped_into_genes[chromosome][gene].append(previous_transcript_id)
+                    transcript_incorporated = 1
+                elif transcript_start <= start <= end <= transcript_end:
+                    transcripts_grouped_into_genes[chromosome][gene].append(previous_transcript_id)
+                    transcript_incorporated = 1
+        if transcript_incorporated == 0:
+            transcripts_grouped_into_genes[chromosome][f"gene_{gene_number}"] = [previous_transcript_id]
+            gene_number+=1
+            
+for chromosome in transcripts_grouped_into_genes:
+    for gene in transcripts_grouped_into_genes[chromosome]:
+        print(chromosome, gene, transcripts_grouped_into_genes[chromosome][gene])
